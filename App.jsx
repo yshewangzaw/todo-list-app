0bb39cc5246f4b0ff60ebb7817ba90033734be97
yshewangzaw <153350220+yshewangzaw@users.noreply.github.com>
2024-02-07 20:10:58 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+import TodoList from './TodoList';
+
+function App() {
+  return (
+    <div>
+      <h1>My Todo App</h1>
+      <TodoList />
+    </div>
+  );
+}
+export default App;
---
+++ TodoList.jsx
@@ -0,0 +1,45 @@
+// src/TodoList.jsx
+import React, { useState } from 'react';
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputValue.trim() !== '') {
+      setTodos([...todos, inputValue.trim()]);
+      setInputValue('');
+    }
+  };
+
+  const handleDeleteTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Add a new todo"
+      />
+      <button onClick={handleAddTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
